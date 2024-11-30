cbd778dc8ecb61be38677bbc5ab2206e50423d3d
akashdip2001 <akashdip.mahapatra.21@aot.edu.in>
2024-11-30 19:54:05 +0530

    fix the essue

--- Project_1/src/main.jsx
+++ Project_1/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>Hello World</StrictMode>
+  <React.StrictMode>
+    <div>Hello World</div>
+  </React.StrictMode>
 );
