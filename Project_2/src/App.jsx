4e31d0ac7c92299c424201b34286ea5f1b2c5302
akashdip2001 <akashdip.mahapatra.21@aot.edu.in>
2024-12-02 05:18:28 +0530

    working

--- Project_2/src/App.jsx
+++ Project_2/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Navigation from './components/Navigation/Navigation'
+import ContactHeader from './components/ContactHeader/ContactHeader'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    // Navigation() is a component that is imported from Navigation.jsx
-    <Navigation/> // Navigation component is called here
-  )
+    <div>
+      {/* // Navigation() is a component that is imported from Navigation.jsx */}
+      <Navigation /> {/*// Navigation component is called here*/}
+      <ContactHeader />
+    </div>
+  );
 }
 
 export default App
---
+++ Project_2/src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,12 @@
+import styles from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  return (
+    <section className={`${styles.box} container`}>
+      <div className={styles.box_form}></div>
+      <div className={styles.box_Img}></div>
+    </section>
+  );
+};
+
+export default ContactForm;
